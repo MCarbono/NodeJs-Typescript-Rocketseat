d27b92617cb653a5d76239c51c585906fb1a8465
Marcelo <carbono.marcelo@gmail.com>
2020-07-30 19:06:12 -0300

    first commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,63 @@
-// import AppError from '../errors/AppError';
-
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+    const transactionRepository = getRepository(Transaction);
+
+    const checkCategoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExists) {
+      const createCategory = await categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(createCategory);
+    }
+
+    if (['income', 'outcome'].includes(type)) {
+      const repo = new TransactionsRepository();
+      const { total } = await repo.getBalance();
+
+      if (type === 'outcome' && total < value)
+        throw new AppError('You do not have enough balance', 400);
+
+      const categoryId = await categoryRepository.findOne({
+        where: { title: category },
+      });
+
+      const transaction = transactionRepository.create({
+        title,
+        value,
+        type,
+        category_id: categoryId.id,
+      });
+
+      await transactionRepository.save(transaction);
+
+      return transaction;
+    }
+
+    throw new AppError('only income or outcome are valid operations', 400);
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = await transactionRepository.findByIds([id]);
+
+    if (!transaction) throw new AppError('Invalid Id', 401);
+
+    await transactionRepository.remove(transaction);
   }
 }
 
